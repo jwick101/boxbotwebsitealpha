d8240cefe5b08406f652e90c53049f6edcfd757d
jwick101 <zac.hersh@gmail.com>
2025-02-10 16:37:20 +0000

    commit "back end build"

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`, // Update to match Infura URL
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
+//
